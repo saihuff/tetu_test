d01209b3b3df2dd6819571c27fc867baaba0c737
saihuff <fumi1take2.s4@gmail.com>
2025-12-19 17:15:41 +0900

    add time

--- app/Main.hs
+++ app/Main.hs
@@ -7,12 +7,21 @@ import Lib
 import qualified Network.Wai as Wai
 import qualified Network.Wai.Handler.Warp as Warp
 import qualified Network.HTTP.Types.Status as Status
-import Data.ByteString.Internal
+import Data.ByteString.Lazy.Internal
+import qualified Data.ByteString.Char8 as BS
+import Data.Text (Text)
+import Data.Time.LocalTime
 
 simpleApp :: Wai.Application
 simpleApp req respond = do
     putStrLn . show . Wai.pathInfo $ req -- とりあえずリクエストのパスとか表示してみる。
-    respond $ Wai.responseLBS Status.status200 [] "test"
+    let rpath = Wai.pathInfo req
+    zt <- getZonedTime
+    case rpath of
+    --respond $ Wai.responseLBS Status.status200 [] "test"
+        ["api"] -> respond $ Wai.responseLBS Status.status200 [] "api"
+        ["time"] -> respond $ Wai.responseLBS Status.status200 [] ((BS.fromStrict . BS.pack . show) zt)
+        otherwise ->  respond $ Wai.responseLBS Status.status200 [] "baka"
 
 main :: IO ()
 main = do
